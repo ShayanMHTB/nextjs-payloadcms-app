6307f7ae4c53a35b6022da386bd9a6a4dab8c72a
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-02-01 09:28:04 +0100

    Template component modified

--- components/layout/Template/index.tsx
+++ components/layout/Template/index.tsx
@@ -1,13 +1,21 @@
-import React, { Fragment } from "react";
-import Footer from "../Footer";
+import React, { Fragment } from 'react';
+import Footer from '../Footer';
+import { Type as FooterType } from '../../../globals/Footer';
+import { Type as SocialMediaType } from '../../../globals/SocialMedia';
 
-const Template: React.FC = ({ children }) => {
-    return (
-        <Fragment>
-            {children}
-            <Footer />
-        </Fragment>
-    );
+type Props = {
+  footer: FooterType
+  socialMedia: SocialMediaType
 }
+
+const Template: React.FC<Props> = ({ children, footer, socialMedia }) => (
+  <Fragment>
+    {children}
+    <Footer
+      footer={footer}
+      socialMedia={socialMedia}
+    />
+  </Fragment>
+);
 
 export default Template;
