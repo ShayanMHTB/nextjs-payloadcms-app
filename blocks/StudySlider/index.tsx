6bca0f3ac57c21eb2f2507b1c85d3d57fd13e891
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-02-02 00:56:21 +0100

    StudySlider block generated

--- blocks/StudySlider/index.tsx
+++ blocks/StudySlider/index.tsx
@@ -1,31 +1,29 @@
 import { Block } from 'payload/types';
-import 
-    backgroundColor, 
-    { Type as BackgroundColorType } from '../../fields/backgroundColor';
+import backgroundColor, { Type as BackgroundColorType } from '../../fields/backgroundColor';
 import { Type as StudyType } from '../../collections/Study';
 
 export type Type = {
-    backgroundColor: BackgroundColorType
-    studies: StudyType[],
-    blockType: 'study-slider'
+  backgroundColor: BackgroundColorType
+  studies: StudyType[]
+  blockType: 'study-slider'
 }
 
 const StudySlider: Block = {
-    slug: 'study-slider',
-    labels: {
-        singular: 'Study Slider',
-        plural: 'Study Sliders',
+  slug: 'study-slider',
+  labels: {
+    singular: 'Study Slider',
+    plural: 'Study Sliders',
+  },
+  fields: [
+    backgroundColor,
+    {
+      name: 'studies',
+      type: 'relationship',
+      relationTo: 'studies',
+      hasMany: true,
+      required: true,
     },
-    fields: [
-        backgroundColor,
-        {
-            name: 'studies',
-            type: 'relationship',
-            relationTo: 'studies',
-            hasMany: true,
-            required: true,
-        },
-    ],
+  ],
 };
 
 export default StudySlider;
