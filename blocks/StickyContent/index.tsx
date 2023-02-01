66abbaf69fd5d61b225a44fbdcbfc26100955d6b
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-02-02 00:56:09 +0100

    StickyContent block generated

--- blocks/StickyContent/index.tsx
+++ blocks/StickyContent/index.tsx
@@ -1,44 +1,44 @@
 import { Block } from 'payload/types';
 
 export type Section = {
-    label: string
-    description: unknown
+  label: string
+  description: string
 }
 
 export type Type = {
-    sections: Section[],
-    blockType: 'sticky-content'
+  sections: Section[]
+  blockType: 'sticky-content'
 }
 
 const StickyContent: Block = {
-    slug: 'sticky-content',
-    labels: {
-        singular: 'Sticky Content Block',
-        plural: 'Sticky Content Blocks',
-    },
-    fields: [
+  slug: 'sticky-content',
+  labels: {
+    singular: 'Sticky Content Block',
+    plural: 'Sticky Content Blocks',
+  },
+  fields: [
+    {
+      name: 'sections',
+      label: 'Sections',
+      type: 'array',
+      minRows: 2,
+      maxRows: 8,
+      fields: [
         {
-            name: 'sections',
-            label: 'Sections',
-            type: 'array',
-            minRows: 2,
-            maxRows: 8,
-            fields: [
-                {
-                    type: 'text',
-                    name: 'label',
-                    label: 'Label',
-                    required: true,
-                },
-                {
-                    type: 'textarea',
-                    name: 'description',
-                    label: 'Description',
-                    required: true,
-                },
-            ],
+          type: 'text',
+          name: 'label',
+          label: 'Label',
+          required: true,
         },
-    ],
+        {
+          type: 'textarea',
+          name: 'description',
+          label: 'Description',
+          required: true,
+        },
+      ],
+    },
+  ],
 };
 
 export default StickyContent;
