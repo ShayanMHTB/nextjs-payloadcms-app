55a319f4d8cde3ea089a8b546b9cf71bb1cc724b
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-02-02 01:03:31 +0100

    pages modified

--- pages/_document.js
+++ pages/_document.js
@@ -1,41 +1,37 @@
-import React from "react";
-import Document from "next/document";
-import {
-    jss,
-    SheetsRegistry,
-    JssProvider,
-    createGenerateId
-} from 'react-jss';
+import React from 'react';
+import Document from 'next/document';
+import { jss, SheetsRegistry, JssProvider, createGenerateId } from 'react-jss';
 import globalPlugin from 'jss-global';
 
+// eslint-disable-next-line import/no-default-export
 export default class JssDocument extends Document {
-    static async getInitialProps(ctx) {
-        const registry = new SheetsRegistry();
-        const generateId = createGenerateId();
-        jss.use(globalPlugin());
-        const originalRenderPage = ctx.renderPage;
+  static async getInitialProps(ctx) {
+    const registry = new SheetsRegistry();
+    const generateId = createGenerateId();
+    jss.use(globalPlugin());
+    const originalRenderPage = ctx.renderPage;
 
-        ctx.renderPage = () => originalRenderPage({
-            enhanceApp: (App) => (props) => (
-                <JssProvider
-                    registry={registry}
-                    generateId={generateId}
-                >
-                    <App {...props} />
-                </JssProvider>
-            ),
-        });
+    ctx.renderPage = () => originalRenderPage({
+      enhanceApp: (App) => (props) => (
+        <JssProvider
+          registry={registry}
+          generateId={generateId}
+        >
+          <App {...props} />
+        </JssProvider>
+      ),
+    });
 
-        const intialProps = await Document.getInitialProps(ctx);
+    const initialProps = await Document.getInitialProps(ctx);
 
-        return {
-            ...intialProps,
-            styles: (
-                <React.Fragment>
-                    {this.getInitialProps.styles}
-                    <style id="server-side-styles">{registry.toString()}</style>
-                </React.Fragment>
-            ),
-        };
-    }
+    return {
+      ...initialProps,
+      styles: (
+        <React.Fragment>
+          {initialProps.styles}
+          <style id="server-side-styles">{registry.toString()}</style>
+        </React.Fragment>
+      ),
+    };
+  }
 }
