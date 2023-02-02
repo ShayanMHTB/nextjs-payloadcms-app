ccad328e3bef5849fa0fdc68a9915aa3c94619d1
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-02-02 01:01:50 +0100

    fields files modified

--- fields/meta.ts
+++ fields/meta.ts
@@ -1,34 +1,32 @@
-import { Field } from "payload/types";
+import { Field } from 'payload/types';
 
 export type Type = {
-    meta?: {
-        title?: string
-        description?: string
-        keywords?: string
-    }
-};
+  title?: string
+  description?: string
+  keywords?: string
+}
 
 const meta: Field = {
-    name: 'meta',
-    label: 'Meta',
-    type: 'group',
-    fields: [
-        {
-            name: 'title',
-            label: 'Title',
-            type: 'text',
-        },
-        {
-            name: 'description',
-            label: 'Description',
-            type: 'textarea',
-        },
-        {
-            name: 'keywords',
-            label: 'Keywords',
-            type: 'text',
-        },
-    ],
+  name: 'meta',
+  label: 'Meta',
+  type: 'group',
+  fields: [
+    {
+      name: 'title',
+      label: 'Title',
+      type: 'text',
+    },
+    {
+      name: 'description',
+      label: 'Description',
+      type: 'textarea',
+    },
+    {
+      name: 'keywords',
+      label: 'Keywords',
+      type: 'text',
+    },
+  ],
 };
 
 export default meta;
